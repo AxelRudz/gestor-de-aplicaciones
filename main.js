9f133bb80e1bd8f288a92f6ec259e9e22aeff602
Axel Rudz <axelrudz@gmail.com>
2024-07-19 01:01:14 -0300

    Agrego persistencia de aplicaciones

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 const path = require('path');
 const { matarTodasLasApps } = require('./electron/AppsDefecto');
+const fs = require('fs');
 
 // Demas funciones de la app
 require('./electron/AppsDefecto');
@@ -70,3 +71,76 @@ ipcMain.handle('seleccionar-directorio', async (event) => {
     }
   });
 });
+
+ipcMain.handle('recuperar-aplicaciones-guardadas', async (event) => {
+  return new Promise(async (resolve, reject) => {
+    const path = app.getPath('userData');
+    console.log("PATHHHHHHHHHHHHHH: ", path);
+    try {
+      const buf = await fs.promises.readFile(`${path}/ListadoAplicacionesGuardadas`, 'utf-8');
+      const apps = buf.split('\n').filter(linea => linea);
+      console.log("Devuelvo las apps: ", apps);
+      resolve(apps)
+    }
+    catch(error){
+      reject(null);
+    }
+  });
+});
+
+ipcMain.handle('persistencia-agregar-aplicacion', async (event, linea) => {
+  return new Promise(async (resolve, reject) => {
+    const path = app.getPath('userData');
+    const filePath = `${path}/ListadoAplicacionesGuardadas`;
+    try {
+      // Leer el archivo para asegurarse de que existe
+      await fs.promises.readFile(filePath, 'utf-8');
+      // Agregar la nueva línea al final del archivo
+      await fs.promises.appendFile(filePath, `${linea}\n`);
+      resolve(true)
+    }
+    catch(error){
+      // Si el archivo no existe, crearlo y agregar la línea
+      if (error.code === 'ENOENT') {
+        try {
+          await fs.promises.writeFile(filePath, linea);
+          resolve(true);
+        } catch (writeError) {
+          reject(writeError);
+        }
+      } else {
+        reject(error);
+      }
+    }
+  });
+});
+
+ipcMain.handle('persistencia-eliminar-aplicacion', async (event, puerto) => {
+  return new Promise(async (resolve, reject) => {
+    const path = app.getPath('userData');
+    const filePath = `${path}/ListadoAplicacionesGuardadas`;
+    try {
+      // Leer el archivo para obtener todas las líneas. Elimino las lineas en blanco
+      const data = await fs.promises.readFile(filePath, 'utf-8');
+      const lineasArchivo = data.split('\n').filter(linea => linea);
+
+      // Filtrar las líneas para eliminar la que contiene el puerto
+      const lineasFiltradas = lineasArchivo.filter(linea => linea.split("|||")[2] != puerto);
+
+      // Sobrescribir el archivo con las líneas restantes
+      await fs.promises.writeFile(filePath, lineasFiltradas.join('\n'));
+
+      resolve(true);
+    } catch (error) {
+      // Si el archivo no existe, resolver con true porque no hay nada que borrar
+      if (error.code === 'ENOENT') {
+        resolve(true);
+      } else {
+        reject(error);
+      }
+    }
+  });
+});
+
+
+
--- src/app/services/aplicacion.service.ts
+++ src/app/services/aplicacion.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Aplicacion } from '../modelo/aplicaciones/Aplicacion';
 import { Angular } from '../modelo/aplicaciones/Angular';
 import { ElectronService } from './electron.service';
+import { SpringBoot } from '../modelo/aplicaciones/SpringBoot';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +11,81 @@ import { ElectronService } from './electron.service';
 export class AplicacionService {  
 
   private aplicaciones: Aplicacion[] = [
-    new Angular("Prueba", 5000, "C:\\Proyectos\\prueba", this.electronService, this)
+    //new Angular("Prueba", 5000, "C:\\Proyectos\\prueba", this.electronService, this)
   ];
 
   private aplicacionesSubject: BehaviorSubject<Aplicacion[]> = new BehaviorSubject<Aplicacion[]>(this.aplicaciones);
-  public aplicaciones$: Observable<Aplicacion[]> = this.aplicacionesSubject.asObservable();  
+  public aplicaciones$: Observable<Aplicacion[]> = this.aplicacionesSubject.asObservable();
 
-  constructor(private electronService: ElectronService){}
+  constructor(private electronService: ElectronService){
+    this.recuperarAplicacionesGuardadas();
+  }
+
+  recuperarAplicacionesGuardadas(){
+    this.electronService.invoke('recuperar-aplicaciones-guardadas')
+      .then((apps: string[]) => {
+        const listadoGeneradoDeApps: Aplicacion[] = [];
+        apps.forEach(app => {
+          const atributos = app.split("|||");
+          const tipo = atributos[0];
+          const nombre = atributos[1];
+          const puerto = atributos[2];
+          const ruta = atributos[3];
+
+          if(tipo == "Angular"){
+            listadoGeneradoDeApps.push(new Angular(nombre, parseInt(puerto), ruta, this.electronService, this))
+          }
+          if(tipo == "Spring-Boot"){
+            listadoGeneradoDeApps.push(new SpringBoot(nombre, parseInt(puerto), ruta, this.electronService, this))
+          }
+        });
+        this.aplicaciones = listadoGeneradoDeApps;
+        this.aplicacionesSubject.next(this.aplicaciones);
+      })
+      .catch(error => {
+        console.log("Error al leer el archivo de configuracion: ", error);
+      })
+  }
+
+  persistenciaAgregarAplicacion(app: Aplicacion){
+    let tipo;
+    if(app instanceof Angular){
+      tipo = "Angular"
+    }
+    if(app instanceof SpringBoot){
+      tipo = "Spring-Boot"
+    }
+    if(!tipo){
+      return;
+    }
+    const linea = `${tipo}|||${app.getNombre()}|||${app.getPuerto()}|||${app.getRuta()}`
+    
+    this.electronService.invoke('persistencia-agregar-aplicacion', linea)
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  persistenciaEliminarAplicacion(nroPuerto: number){
+    const puerto = nroPuerto.toString();
+    this.electronService.invoke('persistencia-eliminar-aplicacion', puerto)
+      .catch(error => {
+        console.error(error);
+      })
+  }
 
   agregarAplicacion(app: Aplicacion): boolean {
     if(this.aplicaciones.some(appAgregada => appAgregada.getPuerto() == app.getPuerto())){
       return false;
     }
+    this.persistenciaAgregarAplicacion(app);
     this.aplicaciones.push(app);
     this.aplicacionesSubject.next(this.aplicaciones);
     return true;
   }
 
   eliminarAplicacion(puerto: number){
+    this.persistenciaEliminarAplicacion(puerto);
     this.aplicaciones = this.aplicaciones.filter(appAgregada => appAgregada.getPuerto() != puerto);
     this.aplicacionesSubject.next(this.aplicaciones);
   }
